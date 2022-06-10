3a87de0d3c5b3b53117f08d1d6a40f40b3a860e5
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-06-10 12:08:24 -0600

    Array unit to use multiple data types

---
+++ src/arrays.js
@@ -0,0 +1,26 @@
+
+/**
+ * List user ages
+ * @type {Array}
+ */
+const ages = [19, 29, 30, 35, 45, {}, true ];
+
+
+/**
+ * Array of user's scores
+ * @type {Array<Number>}
+ */
+const scores = [10, 199.99, 299.99, 3000]
+
+/**
+ * Array of Number and strings
+ * @type {Array<Number | string>}
+ */
+const scoresAndString = [10, "10", "30", 3000]
+
+/**
+ * Array of names
+ * @type {Array<string>}
+ */
+
+const names = ["Ryan", "John", "Joe"]
