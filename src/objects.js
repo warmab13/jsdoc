466487bc6ca7ff86f4d73bfa4e1348481d8d7c7a
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-06-10 12:20:26 -0600

    Object documentation

---
+++ src/objects.js
@@ -0,0 +1,20 @@
+/**
+ * Person Object
+ * @type {{id:Number, firstName:String, lastName:string, age:Number}}
+ */
+const person1 = {
+    id:1,
+    firstName: "Alonso",
+    lastName: "Ray",
+    age: 31
+}
+/**
+ * Person Object
+ * @type {{id:Number | string, firstName:String, lastName:string, age: number|string}}
+ */
+const person2 = {
+    id:1,
+    firstName: "Alonso",
+    lastName: "Ray",
+    age:"30"
+}
