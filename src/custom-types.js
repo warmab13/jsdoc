709a18ded6a83b02bb65c7544e70f7213b4b0e58
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-06-10 12:56:30 -0600

    Custom Types Documentation

---
+++ src/custom-types.js
@@ -0,0 +1,28 @@
+/**
+ * A new User Object
+ * @typedef {Object} User
+ * @property {number} id User Id
+ * @property {string} name User's Name
+ * @property {number | string} [age] User's age (Optional)
+ * @property {boolean} isActive User state
+ */
+
+/**
+ * @type {User}
+ */
+const myNewUser = {
+    id:1,
+    name: 'ryan',
+    age: 30,
+    isActive: true
+}
+
+/**
+ * @type {User}
+ */
+const myNewUser2 = {
+    id:2,
+    name: 'joe',
+    age: 30,
+    isActive: true
+}
