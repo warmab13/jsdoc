0e6f74f3c90fdfe1b63a3bddb8884d749e1911c8
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-06-10 13:24:02 -0600

    Module documentation and example to use module

---
+++ src/example.js
@@ -0,0 +1,4 @@
+const myMath = require('./libs/math');
+
+const data = myMath.addt(2,3);
+console.log(data);
---
+++ src/libs/math.js
@@ -0,0 +1,24 @@
+/**
+ * My math library
+ * @module Math
+ */
+/**
+ * Addition of two numbers
+ * @param {number} x First Number
+ * @param {number} y Second Number
+ * @returns {number} x and y addition
+ */
+const addt = (x, y) => x + y;
+
+/**
+ * Substract two numbers
+ * @param {number} x first number
+ * @param {number} y second number
+ * @returns {number} Subsctract two numbers
+ */
+const substract = (x, y) => x + y;
+
+module.exports = {
+    addt,
+    substract
+}
